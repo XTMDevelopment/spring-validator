39b6ddc9a9b7652ad6f01414c0d6dd24a5a14182
Auriga Aristo <aurigaaristo@gmail.com>
2026-06-13 00:20:58 +0700

    :bug: fix: suppress warnings

--- validator-web/src/test/java/id/xtramile/validator/web/DefaultErrorEnvelopeBuilderTest.java
+++ validator-web/src/test/java/id/xtramile/validator/web/DefaultErrorEnvelopeBuilderTest.java
@@ -278,7 +278,7 @@ class DefaultErrorEnvelopeBuilderTest {
         assertThat(errorsArray).hasSize(100);
     }
 
-    @SuppressWarnings({"unchecked"})
+    @SuppressWarnings({"unchecked", "rawtypes", "RedundantSuppression"})
     @Test
     void shouldBeThreadSafe() throws InterruptedException {
         // Given
